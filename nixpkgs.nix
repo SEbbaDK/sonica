45c6809273facd8fbfddf383f47e241d51afa0f4
SEbbaDK <sebbadk@gmail.com>
2021-10-31 15:33:42 +0100

    nixpkgs: Update

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
     rev = "930da485d9af8100f8858bd6fe8f28e3eca26933";
-    hash = "1a5kazsi00bwmsrj75paif9hyrkdnm61gll1qqs2mwyfmhhcllj6";
+    hash = "1x1ksjz6ag83nnsc1z8liw5sbgxi95lk1j58k3wnjnsx3dhca961";
 in
 import (fetchTarball {
     url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
