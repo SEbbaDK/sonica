95f876ad2fa44a39f5dcecd695b7d8d03b404cd6
Julian T <julian@jtle.dk>
2021-12-01 17:28:39 +0100

    Add helper method for mapping grpc message object to dict

--- gateway/methods.py
+++ gateway/methods.py
@@ -1,4 +1,5 @@
 from validate import validate
+from typing import Tuple, List
 
 import sonica_pb2 as pb2
 
@@ -18,25 +19,34 @@ def api_method(name: str, scheme = None):
     return decor
 
 
+# The scheme is in the form (names, func)
+# where names is either a list of names or a single name,
+# and func maps from message field value to a serializable value
+def dict_from_msg(msg, scheme):
+    res = {}
+    for (names, func) in scheme:
+        # Map a single value to a list with said value
+        names = [names] if type(names) is not list else names
+        # Map None to id function
+        func = (lambda x: x) if func is None else func
+
+        for name in names:
+            res[name] = func(getattr(msg, name))
+    return res
+
+
 def dict_from_song(song: pb2.Song):
-    return {
-        "title": song.title,
-        "artist": song.artist,
-        "album": song.album,
-    }
+    return dict_from_msg(song, [
+        (["title", "artist", "album"], None)
+    ])
 
 
 @api_method("status", {"queue_max": int, "autoplay_max": int})
 def status(sonica, socket, value):
-    print("Status", value)
     resp = sonica.Status(pb2.Status.Query(**value))
-    return "status/info", {
-        "current": dict_from_song(resp.current),
-        "length": resp.length,
-        "progress": resp.progress,
-        "queue_length": resp.queue_length,
-        "queue_hash": resp.queue_hash,
-        "queue": [dict_from_song(s) for s in resp.queue],
-        "autoplay": [dict_from_song(s) for s in resp.autoplay],
-    }
+    return "status/info", dict_from_msg(resp, [
+        ("current", dict_from_song),
+        (["length", "progress", "queue_length", "queue_hash"], None),
+        (["queue", "autoplay"], lambda x: [dict_from_song(s) for s in x])
+    ])
 
