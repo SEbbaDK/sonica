aa6838f780ed6ba546be069309fb0547948853bb
Julian T <julian@jtle.dk>
2021-12-05 22:52:54 +0100

    Build gateway with nix

--- default.nix
+++ default.nix
@@ -2,10 +2,28 @@
 let
 	call = p: (import p) { inherit pkgs; };
 in
-{
+rec {
 	daemon = call ./daemon;
 
 	prepare = call ./prepare;
+
+	python_grpc = pkgs.stdenv.mkDerivation {
+		name = "sonica-python-grpc";
+		src = ./sonica.proto;
+		buildInputs = [ prepare ];
+		dontUnpack = true;
+
+		buildPhase = ''
+			cp $src sonica.proto
+			sonica-prepare \
+				--protofile sonica.proto \
+				generate-grpc-python ./
+		'';
+		installPhase = ''
+			mkdir -p $out/lib
+			cp *.py $out/lib
+		'';
+	};
 
 	# Clients
 	cli = call ./cli;
--- gateway/default.nix
+++ gateway/default.nix
@@ -3,16 +3,47 @@
 , python ? pkgs.python39
 }:
 
-mkDerivation rec {
-	name = "sonica-gateway";
-
-	pythonPackages = python.withPackages (pkg: with pkg; [
+let
+	sonica = import ../. { inherit pkgs; };
+	pythonWithPackages = python.withPackages (pkg: with pkg; [
 		grpcio
 		websockets
 		jsonschema
 	]);
+in
+mkDerivation rec {
+	name = "sonica-gateway";
 
 	nativeBuildInputs = [
-		pythonPackages
+		pythonWithPackages
 	];
+
+	src = (builtins.filterSource
+		(path: type: builtins.elem (baseNameOf path) [
+			"gateway.py"
+			"methods.py"
+			"validate.py"
+		])) ./.;
+
+	dontUnpack = true;
+	dontBuild = true;
+
+	wrapper = pkgs.writeText "wrapper" ''
+#!/bin/sh
+export PYTHONPATH=${sonica.python_grpc}/lib
+${pythonWithPackages}/bin/python ..cd /lib/gateway.py $@
+'';
+
+	installPhase = ''
+		mkdir -p $out/lib
+		cp $src/*.py $out/lib
+
+		mkdir -p $out/bin
+		cat > $out/bin/sonicagw << END_SCRIPT
+#!/usr/bin/bash
+export PYTHONPATH=${sonica.python_grpc}/lib
+${pythonWithPackages}/bin/python $out/lib/gateway.py $@
+END_SCRIPT
+		chmod +x $out/bin/sonicagw
+	'';
 }
