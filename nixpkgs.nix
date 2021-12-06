a17887abb573a101a75198ced6d9c93c7f6af55e
SEbbaDK <sebbadk@gmail.com>
2021-12-06 20:29:21 +0100

    Fix hash of pinned nixpkgs version

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
     rev = "716815ce2a1fcb135843c7441648a59d62fb6eb6";
-    hash = "1x1ksjz6ag83nnsc1z8liw5sbgxi95lk1j58k3wnjnsx3dhca961";
+    hash = "0qhjj4c6jx2s2da6zch2mfy0q7iwz3flpik4w4vw7bag3k26ia6k";
 in
 import (fetchTarball {
     url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
