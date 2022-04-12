d90e43a976d5e1b86d0afc048c76f13d562dbf07
Julian T <julian@jtle.dk>
2022-04-12 21:30:40 +0200

    Further mockup
    Does currently not work, but working towards adding some websockets

--- protocol/parse_thing.py
+++ protocol/parse_thing.py
@@ -48,6 +48,13 @@ class M_status_answer(Message):
 
 
 class R_daemon(Role):
+    def local(status_query_handler: Callable[[M_status_query], Coroutine[None]],
+              play_handler: Callable[[M_play], Coroutine[None]]):
+        return R_daemon(False, {
+            M_status_query: status_query_handler,
+            M_play: play_handler,
+        })
+
     def status_query(self,
                      queue_amount: int,
                      autoplay_amount: int,
@@ -61,8 +68,30 @@ class R_daemon(Role):
 
 
 class R_client(Role):
+    def local(play_handler: Callable[[M_play], Coroutine[None]],
+              exit_handler: Callable[[M_exit], Coroutine[None]]):
+        R_client(False, {
+            M_play: play_handler,
+            M_exit: exit_handler,
+        })
+
     def play(self):
         self.converse(M_play(), {})
 
     def exit(self):
         self.converse(M_exit(), {})
+
+
+# DLASKDJSAL
+import sys
+
+
+async def play_handler():
+    pass
+
+
+async def exit_handler():
+    sys.exit(1)
+
+client = R_client(play_handler, exit_handler)
+client.listen("localhost", 4444)
